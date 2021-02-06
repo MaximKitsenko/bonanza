ff9d9273abc00f922cf60ba4559116bd73d6057e
MaximKitsenko[maximkitsenko.github.io] <maxim.kitsenko.r31@gmail.com>
2021-02-06 17:35:13 +0300

    user created event added

--- src/Bonanza.Contracts/Events/UserCreated.cs
+++ src/Bonanza.Contracts/Events/UserCreated.cs
@@ -1,7 +1,25 @@
-﻿namespace Bonanza.Contracts.Events
+﻿using Bonanza.Contracts.ValueObjects;
+using Bonanza.Contracts.ValueObjects.Tenant;
+using Bonanza.Contracts.ValueObjects.User;
+
+namespace Bonanza.Contracts.Events
 {
 	public class UserCreated
 	{
-		
+		public TenantId TenantId { get; }
+
+		public UserId UserId { get; }
+
+		public UserName Name { get; }
+
+		public SysInfo SysInfo { get; }
+
+		public UserCreated(TenantId tenantId, UserId userId, UserName userName, SysInfo sysInfo)
+		{
+			TenantId = tenantId;
+			UserId = userId;
+			Name = userName;
+			SysInfo = sysInfo;
+		}
 	}
 }
