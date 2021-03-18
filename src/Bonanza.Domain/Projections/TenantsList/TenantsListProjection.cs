5154071dc51296331ba3d53ffa7b07d2e1ad2c7a
MaximKitsenko[maximkitsenko.github.io] <maxim.kitsenko.r31@gmail.com>
2021-03-18 22:42:43 +0300

    projection implemented

--- src/Bonanza.Domain/Projections/TenantsList/TenantsListProjection.cs
+++ src/Bonanza.Domain/Projections/TenantsList/TenantsListProjection.cs
@@ -1,26 +1,55 @@
-﻿using Bonanza.Contracts.Events;
+﻿using System;
+using System.Collections.Generic;
+using Bonanza.Contracts.Events;
+using Bonanza.Contracts.ValueObjects.Tenant;
 using Bonanza.Infrastructure;
 
 namespace Bonanza.Domain.Projections.TenantsList
 {
-	public class TenantsListProjection : 
-		Handles<TenantCreated>, 
-		Handles<TenantNameChanged>, 
-		Handles<UserCreated>
+	public class TenantsListProjection :
+		Handles<TenantCreated>,
+		Handles<TenantNameChanged>
 	{
+		readonly IDocumentWriter<TenantId, TenantsListDto> _store;
+
 		public void Handle(TenantCreated message)
 		{
-			throw new System.NotImplementedException();
+			_store.Add(TenantId.CreateSystemId(), () => new TenantsListDto(message.Id, message.Name));
 		}
 
 		public void Handle(TenantNameChanged message)
 		{
-			throw new System.NotImplementedException();
+			_store.UpdateOrThrow(TenantId.CreateSystemId(), v => v.RenameTenantInList(message.Id, message.NewName));
+		}
+	}
+
+	internal class TenantsListDto
+	{
+		public Dictionary<TenantId,TenantName> Tenants { get; }
+
+		public TenantsListDto(TenantId id, TenantName name)
+		{
+			Tenants = new Dictionary<TenantId, TenantName>(){{id, name}};
 		}
 
-		public void Handle(UserCreated message)
+		public TenantsListDto RenameTenantInList(TenantId tId, TenantName tenantName)
 		{
-			throw new System.NotImplementedException();
+			Tenants[tId] = tenantName;
+			return this;
 		}
+	}
+
+	public interface IDocumentWriter<in TKey, TEntity>
+	{
+		TEntity Add(TKey key, Func<TEntity> addFactory);
+		TEntity AddOrUpdate(TKey key, Func<TEntity> addFactory, Func<TEntity, TEntity> update, AddOrUpdateHint hint = AddOrUpdateHint.ProbablyExists);
+		TEntity UpdateOrThrow(TKey key, Func<TEntity, TEntity> update, AddOrUpdateHint hint = AddOrUpdateHint.ProbablyExists);
+		bool TryDelete(TKey key);
+	}
+
+	public enum AddOrUpdateHint
+	{
+		ProbablyExists,
+		ProbablyDoesNotExist
 	}
 }
