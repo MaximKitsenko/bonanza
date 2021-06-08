f675c7ea096f025149d372b270a757f4ce6fb8aa
Maxim Kitsenko <maxim.kitsenko.r31@gmail.com>
2021-06-08 03:41:38 +0300

    code polished

--- src/Bonanza.Storage/IAppendOnlyStore.cs
+++ src/Bonanza.Storage/IAppendOnlyStore.cs
@@ -70,25 +70,29 @@ namespace Bonanza.Storage
 		public byte[] Data;
 	}
 
-	[Serializable]
-	internal class AppendOnlyStoreConcurrencyException : Exception
-	{
-		internal long ExpectedStreamVersion;
+    /// <summary>
+    /// Is thrown internally, when storage version does not match the condition 
+    /// specified in server request
+    /// </summary>
+    [Serializable]
+    public class AppendOnlyStoreConcurrencyException : Exception
+    {
+        public long ExpectedStreamVersion { get; private set; }
+        public long ActualStreamVersion { get; private set; }
+        public string StreamName { get; private set; }
 
-		public AppendOnlyStoreConcurrencyException()
-		{
-		}
+        protected AppendOnlyStoreConcurrencyException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context) { }
 
-		public AppendOnlyStoreConcurrencyException(string message) : base(message)
-		{
-		}
-
-		public AppendOnlyStoreConcurrencyException(string message, Exception innerException) : base(message, innerException)
-		{
-		}
-
-		protected AppendOnlyStoreConcurrencyException(SerializationInfo info, StreamingContext context) : base(info, context)
-		{
-		}
-	}
+        public AppendOnlyStoreConcurrencyException(long expectedVersion, long actualVersion, string name)
+            : base(
+                string.Format("Expected version {0} in stream '{1}' but got {2}", expectedVersion, name, actualVersion))
+        {
+            StreamName = name;
+            ExpectedStreamVersion = expectedVersion;
+            ActualStreamVersion = actualVersion;
+        }
+    }
 }
--- src/Bonanza.Storage/IEventStore.cs
+++ src/Bonanza.Storage/IEventStore.cs
@@ -37,18 +37,52 @@ namespace Bonanza.Storage
 		public List<IEvent> Events = new List<IEvent>();
 	}
 
-	// todo: implement optimistic concurrency exception
-	public class OptimisticConcurrencyException : Exception
-	{
-		internal static Exception Create(long version, long expectedStreamVersion, IIdentity id, List<IEvent> events)
-		{
-			throw new NotImplementedException();
-		}
-	}
+    /// <summary>
+    /// Is thrown by event store if there were changes since our last version
+    /// </summary>
+    [Serializable]
+    public class OptimisticConcurrencyException : Exception
+    {
+        public long ActualVersion { get; private set; }
+        public long ExpectedVersion { get; private set; }
+        public IIdentity Id { get; private set; }
+        public IList<IEvent> ActualEvents { get; private set; }
 
-	// todo: implement optimistic concurrency exception
-	public class RealConcurrencyException : Exception
-	{
+        OptimisticConcurrencyException(string message, long actualVersion, long expectedVersion, IIdentity id,
+            IList<IEvent> serverEvents)
+            : base(message)
+        {
+            ActualVersion = actualVersion;
+            ExpectedVersion = expectedVersion;
+            Id = id;
+            ActualEvents = serverEvents;
+        }
 
-	}
+        public static OptimisticConcurrencyException Create(long actual, long expected, IIdentity id,
+            IList<IEvent> serverEvents)
+        {
+            var message = string.Format("Expected v{0} but found v{1} in stream '{2}'", expected, actual, id);
+            return new OptimisticConcurrencyException(message, actual, expected, id, serverEvents);
+        }
+
+        protected OptimisticConcurrencyException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context) {}
+    }
+
+    /// <summary>
+    /// Is supposed to be thrown by the client code, when it fails to resolve concurrency problem
+    /// </summary>
+    [Serializable]
+    public class RealConcurrencyException : Exception
+    {
+        public RealConcurrencyException() {}
+        public RealConcurrencyException(string message) : base(message) {}
+        public RealConcurrencyException(string message, Exception inner) : base(message, inner) {}
+
+        protected RealConcurrencyException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context) {}
+    }
 }
